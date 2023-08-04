81dbc99d7a23882e7e9a86b4075fe90bd438f59e
QWERTY <qwerty05@mail.ru>
2023-08-04 04:13:36 +0300

    next2 commmit

--- somebody_site/main/admin.py
+++ somebody_site/main/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Main
 # Register your models here.
+
+
+admin.site.register(Main)
+
--- somebody_site/main/models.py
+++ somebody_site/main/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
 # Create your models here.
+
+
+class Main(models.Model):
+    first_name = models.CharField(max_length=50)
+    photo = models.ImageField(upload_to='img_photo')
+
+
